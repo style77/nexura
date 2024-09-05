8b67687a7d2b25770f6167305049dfd8d6d5537f
style77 <stylek777@gmail.com>
2024-09-06 01:37:06 +0200

    chore: basic web server

--- nexura/main.py
+++ nexura/main.py
@@ -1,1 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+import dotenv
+
+dotenv.load_dotenv("../.env")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
+
+app.add_api_route("{provider}/{endpoint}")
